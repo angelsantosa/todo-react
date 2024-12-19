fbbce989fed26279c1c058e07306d1ea85877831
Angel Santos <angel_santos@outlook.com>
2024-12-19 15:13:08 -0300

    todos status column init

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,41 @@
+import { Grid, Paper, Title, type MantineStyleProp } from '@mantine/core';
+
 export function IndexRoute() {
-  return <div>Index Route</div>;
+  const columnStyle: MantineStyleProp = {
+    display: 'flex',
+    alignItems: 'center',
+    flexDirection: 'column',
+  };
+
+  return (
+    <Grid gutter="sm" justify="space-around" align="flex-start">
+      <Paper
+        component={Grid.Col}
+        shadow="xs"
+        span={3}
+        mih={'600'}
+        style={columnStyle}
+      >
+        <Title order={4}>Todo</Title>
+      </Paper>
+      <Paper
+        component={Grid.Col}
+        shadow="xs"
+        span={3}
+        mih={'600'}
+        style={columnStyle}
+      >
+        <Title order={4}>In progress</Title>
+      </Paper>
+      <Paper
+        component={Grid.Col}
+        shadow="xs"
+        span={3}
+        mih={'600'}
+        style={columnStyle}
+      >
+        <Title order={4}>Done</Title>
+      </Paper>
+    </Grid>
+  );
 }
