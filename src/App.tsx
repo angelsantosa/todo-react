7c05189bbff7653ac731a9ca8f5a091f8bd81465
Angel Santos <angel_santos@outlook.com>
2024-12-19 17:44:52 -0300

    init tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import '@mantine/core/styles.css';
 import { IndexRoute } from './routes';
 import { AppShell, createTheme, MantineProvider, Title } from '@mantine/core';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const theme = createTheme({});
 
 const App = () => {
+  const queryClient = new QueryClient({});
+
   return (
-    <MantineProvider theme={theme}>
-      <AppShell header={{ height: 60 }} padding="lg">
-        <AppShell.Header style={{ padding: 10 }}>
-          <Title order={3}>Welcome To The Every.io Code Challenge.</Title>
-        </AppShell.Header>
-        <AppShell.Main>
-          <IndexRoute />
-        </AppShell.Main>
-      </AppShell>
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider theme={theme}>
+        <AppShell header={{ height: 60 }} padding="lg">
+          <AppShell.Header style={{ padding: 10 }}>
+            <Title order={3}>Welcome To The Every.io Code Challenge.</Title>
+          </AppShell.Header>
+          <AppShell.Main>
+            <IndexRoute />
+          </AppShell.Main>
+        </AppShell>
+      </MantineProvider>
+    </QueryClientProvider>
   );
 };
 
